942d76f65b98e70de565b9ab399ef65e3d11cd0c
RaulPutraWidodo <ultra300405@gmail.com>
2026-01-12 14:37:21 +0700

    Initial commit

---
+++ praktikumBigData-main/reducer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+from itertools import groupby
+
+# Membaca semua baris dari input standar (stdin)
+for key, group in groupby(sys.stdin, key=lambda x: x.split('\t', 1)[0]):
+    try:
+        total_count = sum(
+            int(line.split('\t', 1)[1].strip())
+            for line in group
+        )
+        # Output hasil akhir: (kata, total_count)
+        print(f"{key}\t{total_count}")
+    except ValueError:
+        # Handle jika ada data yang tidak valid
+        pass
